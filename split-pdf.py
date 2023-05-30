e94a55cf678db805e1916cf3374e6d7f2483d8bb
nicolas <nicolas.seiller@protonmail.com>
2023-05-31 00:23:31 +0200

    count pages as they are generated

--- split-pdf.py
+++ split-pdf.py
@@ -28,11 +28,9 @@ parser.add_argument(
 )
 
 
-def write_single_page(page, pdf, path, output_folder):
+def write_single_page(page, pdf, output_path):
     pdf_writer = PdfWriter()
     pdf_writer.add_page(pdf.pages[page])
-
-    output_path = output_folder / f"{path.name.replace('.pdf', '')}_page_{page+1}.pdf"
 
     write_pdf(output_path, pdf_writer)
 
@@ -44,46 +42,54 @@ def write_pdf(path, writer):
     print("Created: {}".format(path.name))
 
 
-def write_split_pages(page_index, pdf, path, output_folder):
-    page = pdf.pages[page_index]
-    dimensions = (page.mediabox.right / 2, page.mediabox.top)
-    output_file_name_prefix = f"{path.name.replace('.pdf', '')}_page_"
-
+def generate_left_page(page, dimensions):
     pdf_writer = PdfWriter()
     output_page = pdf_writer.add_blank_page(page.mediabox.width, page.mediabox.height)
     output_page.merge_page(page)
     output_page.mediabox.upper_right = dimensions
-    output_page_index = page_index * 2 + 1
-    output_path = output_folder / f"{output_file_name_prefix}{output_page_index}.pdf"
+    return pdf_writer
 
-    write_pdf(output_path, pdf_writer)
 
+def generate_right_page(page, dimensions):
     pdf_writer = PdfWriter()
     output_page = pdf_writer.add_blank_page(page.mediabox.width, page.mediabox.height)
     output_page.merge_page(page)
     output_page.mediabox.upper_left = dimensions
-    output_page_index = (page_index + 1) * 2
-    output_path = output_folder / f"{output_file_name_prefix}{output_page_index}.pdf"
-
-    write_pdf(output_path, pdf_writer)
+    return pdf_writer
 
 
-def split_pdf(path, split_pages):
+def split_pdf(path):
     pdf = PdfReader(path)
     output_folder = path.parent.absolute() / "single_page_export"
     output_folder.mkdir(exist_ok=True)
+    widths = {p.mediabox.width for p in pdf.pages}
+    max_width = max(widths)
+    output_file_count = 0
     for page_index in range(len(pdf.pages)):
-        if split_pages:
-            write_split_pages(page_index, pdf, path, output_folder)
+        output_file_name_prefix = f"{path.name.replace('.pdf', '')}_page_"
+        output_file_count = output_file_count + 1
+        output_path = output_folder / f"{output_file_name_prefix}{output_file_count}.pdf"
+        if len(widths) > 1 and pdf.pages[page_index].mediabox.width == max_width:
+            page = pdf.pages[page_index]
+            dimensions = (page.mediabox.right / 2, page.mediabox.top)
+
+            writer = generate_left_page(page, dimensions)
+            write_pdf(output_path, writer)
+
+            output_file_count = output_file_count + 1
+            output_path = output_folder / f"{output_file_name_prefix}{output_file_count}.pdf"
+            writer = generate_right_page(page, dimensions)
+            write_pdf(output_path, writer)
+
         else:
-            write_single_page(page_index, pdf, path, output_folder)
+            write_single_page(page_index, pdf, output_path)
 
 
 def main():
     args = parser.parse_args()
     files = args.paths
     for one_file in files:
-        split_pdf(one_file, True)
+        split_pdf(one_file)
 
 
 if __name__ == "__main__":
